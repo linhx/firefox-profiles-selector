d54c181e8360611ee569dc97fff2019d7756e863
Nguyen Dinh Linh <50461902+linhx@users.noreply.github.com>
2023-11-27 00:55:02 +0700

    fix: can't start firefox

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ tee -a ~/.local/share/applications/firefox-profiles-selector.desktop <<EOF
 Version=1.0
 Name=Firefox profiles selector
 Comment=Firefox profiles selector
-Exec=$(pwd)/firefox-profiles-selector %u
+Exec=bash -i -c '$(pwd)/firefox-profiles-selector %u'
 Terminal=false
 Type=Application
 MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/vnd.mozilla.xul+xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;
