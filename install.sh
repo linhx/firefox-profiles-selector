9eef2a9efbeed393e0802f0f88565d1838cc0352
linhx <nguyendinhlinhx@gmail.com>
2024-12-28 13:58:07 +0700

    fix(#2): Cannot login after click Intellij login

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
-
-
-tee -a ~/.local/share/applications/firefox-profiles-selector.desktop <<EOF
+tee ~/.local/share/applications/firefox-profiles-selector.desktop <<EOF
 [Desktop Entry]
 Version=1.0
 Name=Firefox profiles selector
 Comment=Firefox profiles selector
-Exec=bash -i -c '$(pwd)/firefox-profiles-selector %u'
+Exec=bash -i -c '$(pwd)/firefox-profiles-selector "%u"'
 Terminal=false
 Type=Application
 MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/vnd.mozilla.xul+xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;
